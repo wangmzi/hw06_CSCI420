31aae53c63a8547e8933fd9af773562149073104
al7302 <al7302@g.rit.edu>
2021-10-16 21:02:50 -0400

    added exclusion arrays

--- HW06_Lee_Wang_Part_B.py
+++ HW06_Lee_Wang_Part_B.py
@@ -5,12 +5,27 @@ import math as m
 import matplotlib.pyplot as plt
 import csv
 
+#Attribute names
+attr_names = ['ID', 'Milk:0', 'ChdBby:1', 'Vegges:2', 'Cerel:3', 'Bread:4', 'Rice:5', 'Meat:6', 
+'Eggs:7', 'YogChs:8', 'Chips:9', 'Soda:10', 'Fruit:11', 'Corn:12', 'Fish:13', 'Sauce:14', 'Beans:15', 'Tortya:16', 
+'Salt:17', 'Scented:18', 'Salza:19'] 
 
-attr_names = ['ID', 'Milk', 'ChdBby', 'Vegges', 'Cerel', 'Bread', 'Rice', 'Meat', 
-'Eggs', 'YogChs', 'Chips', 'Soda', 'Fruit', 'Corn', 'Fish', 'Sauce', 'Beans', 'Tortya', 
-'Salt', 'Scented', 'Salza']  
+#Typifying based on attribute indexes, putting the indexes to exclude
+#the attribute, not include
+#excluding: meats and fish
+vegetarian = [6,13]
+#excluding: milk,meat,yogurt/cheese, fish
+vegan = [0,6,7,8,13]
+
+#The first iteration should be all data_points as individual clusters
+#There are 880 entries and therefore a maximum of 880 clusters
+max_clusters = 880
+
+
+
 #Takes a guest entry and converts it to a cluster format.
 class cluster:
+    #Constructor
     def __init__(self, guest_id, records):
         #cluster guest Id
         self.id = guest_id
@@ -19,20 +34,22 @@ class cluster:
         #Keeps track of what clusters are merged into the cluster
         self.merged_clusters = []
 
+    #appends a cluster to the list
     def merge(cluster):
         self.merged_clusters.append(cluster)
     
+    #formats print statement
     def __str__(self):
         Id = []
         for x in self.merged_clusters:
             Id.append(x.id)
-        return "Id: "+ str(self.id) + '\nmerged_clusters: '+ str(Id)
+        return ("Id: "+ str(self.id) + 
+        '\nattributes: ' + str(self.records) +
+        '\nmerged_clusters: '+ str(Id)
+        )
 
-
-#used to find distance between two centers of clusters
-def euclid_dist(centerA,centerB):
-    return m.dist(centerA,centerB)
-
+#Gets data from the csv file and organizes it into an array
+#returns an array of entries
 def retrieve_data(csv_filename):
     file = open(csv_filename)
     reader = csv.reader(file)
@@ -48,17 +65,26 @@ def retrieve_data(csv_filename):
         data.append(curr_entry)
     return data
 
+#Takes the array of data and converts each entry into a cluster
+#returns an array of clusters
 def data_to_cluster(data):
     clusters = []
     for x in data:
         clusters.append(cluster(x[0], x[1:]))
     return clusters
 
+#Used to find distance between two centers of clusters
+#returns the distance between tuple A and B OR
+#array A and B
+def euclid_dist(centerA,centerB):
+    return m.dist(centerA,centerB)
+
+
 def main():
     data = retrieve_data('HW_CLUSTERING_SHOPPING_CART_v2211.csv')
     clusters = data_to_cluster(data)
-    print(clusters[0])
-    print(clusters[0].records)
+    print(len(clusters))
+
     return
 if __name__ == '__main__':
     main()

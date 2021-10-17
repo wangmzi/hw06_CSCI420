5ee4312797de1666611edbbd934ba8020e12dc31
al7302 <al7302@g.rit.edu>
2021-10-16 20:12:17 -0400

    organized data into clusters.

--- HW06_Lee_Wang_Part_B.py
+++ HW06_Lee_Wang_Part_B.py
@@ -1,27 +1,65 @@
 #Creator: Vicky Wang, Anthony Lee
 #CSCI 420: HW06
 
-import numpy as np
-from math import dist
+import math as m
 import matplotlib.pyplot as plt
+import csv
 
-#used to find distance between two centers of clusters
-def euclid_dist(centerA,centerB):
-    return dist(centerA,centerB)
 
+attr_names = ['ID', 'Milk', 'ChdBby', 'Vegges', 'Cerel', 'Bread', 'Rice', 'Meat', 
+'Eggs', 'YogChs', 'Chips', 'Soda', 'Fruit', 'Corn', 'Fish', 'Sauce', 'Beans', 'Tortya', 
+'Salt', 'Scented', 'Salza']  
+#Takes a guest entry and converts it to a cluster format.
 class cluster:
-    def __init__(self, center, records, id_members):
-        #coordinate to be the center of cluster.
-        self.center = center
-
-        #Keeps track of all points in cluster
+    def __init__(self, guest_id, records):
+        #cluster guest Id
+        self.id = id
+        #Keeps track of all attributes in cluster
         self.records = records
-
-        #Keeps track of guest id joined in the cluster.
-        self.id_members = id_members
-
         #Keeps track of what clusters are merged into the cluster
         self.merged_clusters = []
 
-    def merge(new_cluster):
-        self.merged_clusters.append(new_cluster)
+    def merge(cluster):
+        self.merged_clusters.append(cluster)
+
+    def info(self):
+        print("Id:", self.id)
+        Id = []
+        for x in self.merged_clusters:
+            Id.append(x.id)
+        print("merged_clusters:",Id)
+
+
+#used to find distance between two centers of clusters
+def euclid_dist(centerA,centerB):
+    return m.dist(centerA,centerB)
+
+def retrieve_data(csv_filename):
+    file = open(csv_filename)
+    reader = csv.reader(file)
+    next(reader)
+    data = []
+    for x in reader:
+        curr_entry = []
+        for val in x:
+            try:
+                curr_entry.append(int(val))
+            except:
+                pass
+        data.append(curr_entry)
+    return data
+
+def data_to_cluster(data):
+    clusters = []
+    for x in data:
+        clusters.append(cluster(x[0], x[1:]))
+    return clusters
+
+def main():
+    data = retrieve_data('HW_CLUSTERING_SHOPPING_CART_v2211.csv')
+    clusters = data_to_cluster(data)
+    clusters[0].info()
+    return
+
+if __name__ == '__main__':
+    main()
